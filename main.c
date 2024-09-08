2bddfa9819218f8725c1e20cb543536745b38b48
eabalestra <agusbalestra0@gmail.com>
2024-09-08 11:56:33 -0300

    fix project structure

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include "SymbolTable.h"
 
-
 int main() {
     SymbolTable *table = createTable();
-    
+
     Node *node1 = createNode(ID, NONTYPE, "x", NULL);
     Node *node2 = createNode(ID, NONTYPE, "y", NULL);
     insert(table, node1);
     insert(table, node2);
-    
+
     printSymbolTable(table);
 
     Node *node3 = createNode(ID, NONTYPE, "x", NULL);
@@ -19,6 +18,6 @@ int main() {
 
     Node *wanted_node = search(table, "x");
     printf("ENCONTRE: %s\n", (char *) wanted_node->value);
-    
+
     return 0;
 }
--- parser/preparser.y
+++ parser/preparser.y
@@ -1,7 +1,7 @@
 %{
 #include <stdlib.h>
 #include <stdio.h>
-#include "binarytree/Tree.h"
+#include "../binarytree/Tree.h"
 
 Tree *ast;
 %}
